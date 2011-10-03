10a9f7285ac066093c3629e3d3cae1d8c19283f0
David Jensen <david.lgj@gmail.com>
2011-10-03 14:43:51 +0200

    Bugfixes and a couple of features  * one tomcat for share and one for repo  ** -t and -ts  * host for share and repo  ** -th and -tsh  * growlnotify for mac (untested)  * -q now works as expected  * -N to turn of notification

--- alfresco.py
+++ alfresco.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #   This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -18,11 +18,16 @@ from subprocess import call
 
 
 class Alfresco(object):
-    def __init__(self,tomcat,share,repo,notification=None):
-        self.tomcat = path.abspath(tomcat)
-        self.share  = path.abspath(share)
-        self.repo   = path.abspath(repo)
-        self.notification = notification
+    def __init__(self,args):
+        self.tomcat = path.abspath(args.tomcat)
+        self.tomcat_share = args.tomcat_share and path.abspath(args.tomcat_share) or path.abspath(args.tomcat) #if not set defualt to repo tomcat
+        self.share  = path.abspath(args.share)
+        self.repo   = path.abspath(args.repo)
+        self.notification = args.no_notification and None or args.notification
+        self.quiet = args.quiet
+        self.host = args.tomcat_host 
+        self.share_host = args.tomcat_share_host
+        
 
     def notify(self,msg):
         """Send out a notification message"""
@@ -30,7 +35,8 @@ class Alfresco(object):
             call(self.notification % msg,shell=True)
 
     def call(self,*args,**kwargs):
-        print "Executing: ",args[0]
+        if not self.quiet:
+            print "Executing: ",args[0]
         call(*args,**kwargs)
 
     def sync(self,scope):
@@ -58,23 +64,23 @@ class Alfresco(object):
 
     def reload_repo(self):
         """ Reload repo webscripts using it's webservice (with curl)"""
-        self.call('curl --user admin:admin --data-urlencode "reset=on" http://localhost:8080/alfresco/service/index > /dev/null 2>&1',shell=True)
+        self.call('curl --user admin:admin --data-urlencode "reset=on" %s/alfresco/service/index %s' %  (self.host,self.quiet and '> /dev/null 2>&1' or ''),shell=True)
 
     def reload_share(self):
         """ Reload share webscripts using it's webservice (with curl)"""
-        self.call('curl --user admin:admin --data-urlencode "reset=all" http://localhost:8080/share/page/console > /dev/null 2>&1',shell=True)
+        self.call('curl --user admin:admin --data-urlencode "reset=all" %s/share/page/console %s' % (self.share_host,self.quiet and '> /dev/null 2>&1' or ''),shell=True)
         
     def sync_repo(self):
         """ rsync repo webscripts"""
-        self.call("rsync -avh %s %s" % (path.join(self.repo,'config/'),path.join(self.tomcat,'shared/classes/')),shell=True)
+        self.call("rsync -avh %s %s %s" % (path.join(self.repo,'config/'),path.join(self.tomcat,'shared/classes/'),self.quiet and '> /dev/null 2>&1' or ''),shell=True)
 
     def sync_webapp(self):
         """ rsync webbapp, i.e. client side js,css and images """
-        self.call("rsync -avh %s %s" % (path.join(self.share,'webapp/'),path.join(self.tomcat,'webapps/share/')),shell=True)
+        self.call("rsync -avh %s %s %s" % (path.join(self.share,'webapp/'),path.join(self.tomcat_share,'webapps/share/'),self.quiet and '> /dev/null 2>&1' or ''),shell=True)
         
     def sync_share(self):
         """ rsync share webscripts"""
-        self.call("rsync -avh %s %s" % (path.join(self.share,'config/'),path.join(self.tomcat,'shared/classes/')),shell=True)
+        self.call("rsync -avh %s %s %s" % (path.join(self.share,'config/'),path.join(self.tomcat_share,'shared/classes/'),self.quiet and '> /dev/null 2>&1' or ''),shell=True)
 
     def sync_all(self):
         """Syncs both repo and share """
@@ -92,56 +98,68 @@ if __name__ == "__main__":
     import sys
     import argparse
 
-    def using(tomcat,repo,share):
+    def using(tomcat,tomcat_share,repo,share):
             print """Using:
 tomcat: %s 
+tomcat share: %s
 repo:   %s
 share:   %s
-        """ % (tomcat,repo,share)
+        """ % (tomcat,tomcat_share or 'Not set',  repo,share)
 
+    #some mac os x love
+    notification_cmd = sys.platform.startswith('darwin') and 'growlnotify -m "%s"' or 'notify-send --hint=int:transient:1 "%s"'
     
     parser = argparse.ArgumentParser(description="Hotdeployment skript for alfresco")
-    parser.add_argument('-q','--quiet' ,help="Disable notfications",default=False)
-    parser.add_argument('-n','--notification',default='notify-send --hint=int:transient:1 "%s"',help="Notification binary, change on mac os x")
-    parser.add_argument('-t','--tomcat',default="../alfresco-3.3.5/tomcat"     ,help="Path to tomcat, default is ../../tomcat")
-    parser.add_argument('-s','--share' ,default="trunk/share/src/main/",help="Path to share src , default is ./share/src/main/")
-    parser.add_argument('-r','--repo'  ,default="trunk/repo/src/main/" ,help="Path to tomcat, default is ./repo/src/main/")
+    parser.add_argument('-q','--quiet',action="store_true", default=False,help="Disable output")
+    parser.add_argument('-N','--no-notification',action="store_true", default=False,help="Disable notfications")
+    parser.add_argument('-n','--notification',default=notification_cmd,help="Notification binary")
+    parser.add_argument('-t','--tomcat',default="/opt/alfresco/tomcat"     ,help="Path to tomcat")
+    parser.add_argument('-ts','--tomcat-share',default=None ,help="Path to tomcat used for share if it's different than repo. Only required when using two tomcats and 'reload' or 'deploy' 'all'")
+    parser.add_argument('-th','--tomcat-host',default="http://localhost:8080",help="Host used for tomcat, i.e. http://localhost:8080")
+    parser.add_argument('-tsh','--tomcat-share-host',default="http://localhost:8080",help="Host used for alfresco share tomcat, i.e. http://localhost:8080")
+    parser.add_argument('-s','--share' ,default="trunk/share/src/main/",help="Path to share src")
+    parser.add_argument('-r','--repo'  ,default="trunk/repo/src/main/" ,help="Path to repo src")
     sub = parser.add_subparsers(help="Action subparser")
 
 
     sync = sub.add_parser('sync',help="Sync files with rsync")
-    sync.add_argument('scope',choices=['repo','share','webapp','all'])
+    sync.add_argument('scope',choices=['repo','share','webapp','all'],help="What to sync, 'share' will also sync webapp and 'all' syncs them all")
+
+
 
     def sync_func(args):
-        using(args.tomcat,args.repo,args.share)
-        a = Alfresco(args.tomcat,args.share,args.repo,not args.quiet and args.notification or None )
+        if not args.quiet:
+            using(args.tomcat,args.tomcat_share,args.repo,args.share)
+        a = Alfresco(args )
         a.sync(args.scope)
         a.notify("Synced %s" % args.scope)
         
     sync.set_defaults(func=sync_func)
 
     rel = sub.add_parser('reload',help="Reload wbescript with curl")
-    rel.add_argument('scope',choices=['repo','share','all'])
+    rel.add_argument('scope',choices=['repo','share','all'],help="What to reload, 'repo','share' or 'all'")
     
     def rel_func(args):
-        using(args.tomcat,args.repo,args.share)
-        a = Alfresco(args.tomcat,args.share,args.repo,not args.quiet and args.notification or None )
+        if not args.quiet:
+            using(args.tomcat,args.tomcat_share,args.repo,args.share)
+        a = Alfresco(args)
         a.reload(args.scope)
         a.notify("Reloaded %s" % args.scope)
 
     rel.set_defaults(func=rel_func)
 
-    both = sub.add_parser('deploy',help="Sync and reload")
-    both.add_argument('scope',choices=['repo','share','all'])
+    deploy = sub.add_parser('deploy',help="Sync and reload")
+    deploy.add_argument('scope',choices=['repo','share','all'],help="What to sync and reload: 'repo','share' or 'all'")
 
-    def both_func(args):
-        using(args.tomcat,args.repo,args.share)
-        a = Alfresco(args.tomcat,args.share,args.repo,not args.quiet and args.notification or None )
+    def deploy_func(args):
+        if not args.quiet:
+            using(args.tomcat,args.tomcat_share,args.repo,args.share)
+        a = Alfresco(args)
         a.sync(args.scope)
         a.reload(args.scope)
         a.notify("Synced and reloaded %s" % args.scope)
 
-    both.set_defaults(func=both_func)
+    deploy.set_defaults(func=deploy_func)
 
     args = parser.parse_args()
     args.func(args)
