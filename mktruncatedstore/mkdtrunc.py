e06526645930ec8a64d46b78cc1855a0828b1fab
Niklas Ekman <niklas.ekman@redpill-linpro.com>
2012-02-21 15:05:32 +0100

    Extended the script to be more automated
    Signed-off-by: Niklas Ekman <niklas.ekman@redpill-linpro.com>

--- mktruncatedstore/mkdtrunc.py
+++ mktruncatedstore/mkdtrunc.py
@@ -12,13 +12,19 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # Copyright 2012, Redpill Linpro
 # Fredrik Steen <fredrik.steen@redpill-linpro.com>
+# Niklas Ekman <niklas.ekman@redpill-linpro.com>
+# David Jensen <david.jensen@redpill-linpro.com>
 
 import sys
 import os.path
 import argparse as arg
+import subprocess as sub
+import urllib2
+import base64
+import shutil
 
 def touch(fname):
     if os.path.exists(fname):
@@ -27,17 +33,19 @@ def touch(fname):
         open(fname, 'w').close()
 
 
-def truncate(args):
+def truncate(args,files):
     # Read from stdin
-    for line in open(args.file).readlines():
+    for line in files.splitlines():
         line = line.strip()
-        # Strip the leading path
-        if args.strip:
-            line = line[len(args.strip):]
-
+        
         # We know that every file in the contentstore is a .bin file,
         # we also weed out empty directories here.
-        if line.find(".bin") != -1:
+        if line.find('.bin') != -1:
+            #strip path up to and including contentstore
+            store = os.path.abspath(args.store)
+            line = line[len(store):]
+            line = line[0] == '/' and line[1:] or line
+            
             # only the path
             tmpdir = os.path.join(args.dest, os.path.dirname(line))
             # Full path to new file
@@ -46,32 +54,85 @@ def truncate(args):
                 # check if we already got the directory.
                 if os.path.isdir(tmpdir) is False:
                     if args.noop:
-                        print "mkdir:  %s" % tmpdir
+                        print 'mkdir:  %s' % tmpdir
                     else:
                          os.makedirs(tmpdir, 0755)
                 # Now we should have our directory so lets create
                 # an empty file.
                 if args.noop:
-                    print "touch:  %s" % tmpfile
+                    print 'touch:  %s' % tmpfile
                 else:
                     touch(tmpfile)
             except os.error, e:
                 # We send errors to stdout
                 print >> sys.stderr. e
 
+def create_list(store):
+    pth = os.path.abspath(store)
+    return sub.Popen('find %s' % pth, shell=True, stdout=sub.PIPE).communicate()[0]
+
+def fetch_list(url, username, password):
+    request = urllib2.Request(url)
+    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+    request.add_header('Authorization', 'Basic %s' % base64string)
+    res = urllib2.urlopen(request, timeout=10*60).read().splitlines()
+    
+    return filter(lambda x: x != '',res)
+
+def copy_files(store, files, dest, noop=False):
+    for f in files:
+        if noop:
+            print 'Copying file %s to %s' % (f, os.path.join(dest, f))
+        else:
+            shutil.copy2(os.path.join(store, f),os.path.join(dest, f))
+    
 def main():
-    p = arg.ArgumentParser("Make a truncated content store")
-    p.add_argument("-f", '--file', help="read input file")
-    p.add_argument("-d", '--dest', help="desitnation base path")
-    p.add_argument("-s", '--strip', help="strip path")
-    p.add_argument("-n", '--noop', action="store_true", help="do nothing, only print")
+    p = arg.ArgumentParser('Make a truncated content store')
+    
+    p.add_argument('-s', '--store', help='path to contentstore')
+    p.add_argument('-d', '--dest', default='/tmp/contentstore',help='desitnation base path')
+    p.add_argument('-n', '--noop', action='store_true', help='do nothing, only print')
+    p.add_argument('-u', '--url', default='http://localhost:8080/alfresco/service/admin/excludelist',help='url to fetch list from webscript')
+    p.add_argument('-U', '--username', default='admin',help='username for authentication')
+    p.add_argument('-P', '--password', default='admin',help='password for authentication')
+    p.add_argument('-nz', '--nogzip', action='store_true',help='do not tar, gzip and clean up afterwards')
+    p.add_argument('-p', '--package', default='contentstore.tar.gz', help='filename to use when creating a tar and gzip package')
+    
     args = p.parse_args()
-    if args.dest is None or args.file is None:
+    if args.dest is None or args.store is None:
         p.print_help()
         sys.exit(0)
-
-    truncate(args)
-
+        
+    # create dest dir if not found
+    print('Checking for destination directory and creating if needed...\n')
+    if not os.path.isdir(args.dest):
+        os.mkdir(os.path.abspath(args.dest))
     
+    # create a list of the files in the contentstore
+    print('Creating list of files to create 0 byte files for...\n')
+    files = create_list(args.store)
+    
+    # create 0 byte files and directories of the files
+    print('Creating 0 byte files...\n')
+    truncate(args,files)
+    
+    # get a list of files to copy from a URL
+    print('Getting list from server representing actual files to copy...\n')
+    files_to_copy = fetch_list(args.url,args.username,args.password)
+    
+    # copy the files to the destination
+    print('Copy the actual files...\n')
+    copy_files(args.store,files_to_copy,args.dest,args.noop)
+    
+    # if gzip is needed, execute that
+    if not args.nogzip:
+        if args.noop:
+            print 'packaging %s' % args.package
+        else:
+            print('Packaging result as tar.gz file and clean up...\n')
+            dest = args.dest[:-1] == '/' and args.dest or args.dest+'/'
+            sub.call('tar -czf %s %s' % (args.package, dest) ,shell=True)
+            sub.call('rm -r %s' % dest,shell=True)
+
 if __name__ == '__main__':
     main()
