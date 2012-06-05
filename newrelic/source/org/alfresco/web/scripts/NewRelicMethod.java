b067cd97ae308616d16eb0b78f67a3eacf6e1c95
Marcus Svensson <marcus.svensson@redpill-linpro.com>
2012-06-05 11:07:22 +0200

    NewRelic module for Alfresco 3.2

--- newrelic/source/org/alfresco/web/scripts/NewRelicMethod.java
+++ newrelic/source/org/alfresco/web/scripts/NewRelicMethod.java
@@ -1,4 +1,3 @@
-package org.springframework.extensions.webscripts;
 /**
     This file is part of NewRelic Module for Alfresco.
 
@@ -15,8 +14,9 @@ package org.springframework.extensions.webscripts;
     You should have received a copy of the GNU Lesser General Public License
     along with NewRelic Module for Alfresco.  If not, see <http://www.gnu.org/licenses/>.
 **/
+package org.alfresco.web.scripts;
 
-import org.springframework.extensions.webscripts.processor.BaseProcessorExtension;
+import org.alfresco.web.scripts.processor.BaseProcessorExtension;
 import org.apache.log4j.Logger;
 
 /**
